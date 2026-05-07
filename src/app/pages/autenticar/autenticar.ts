1b94c1138fbe19a78ea77bfd754c6d31b194c989
MarcioGomes-Dev <marciojsg.dev@gmail.com>
2026-05-06 22:06:00 -0300

    Cadastro, autenticação, sessão, login e logout

--- src/app/pages/autenticar/autenticar.ts
+++ src/app/pages/autenticar/autenticar.ts
@@ -1,9 +1,47 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, signal } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-autenticar',
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './autenticar.html',
   styleUrl: './autenticar.css',
 })
-export class Autenticar {}
+export class Autenticar {
+
+   http = inject(HttpClient);
+
+   mensagemErro = signal<string>('');
+
+  formAutenticar = new FormGroup({
+    email: new FormControl('', [Validators.required]),
+    senha: new FormControl('', [Validators.required])    
+  });
+
+  autenticar() {
+    
+    this.mensagemErro.set('');
+
+    this.http.post('http://localhost:8082/api/usuario/autenticar',this.formAutenticar.value)
+    .subscribe({
+      next: (data: any) => {
+        
+        this.formAutenticar.reset();  
+        
+        sessionStorage.setItem('usuario', JSON.stringify(data));
+
+        location.href = "/dashboard";
+      },
+      error: (e) => {
+        this.mensagemErro.set(e.error);
+      }
+    });
+  }
+
+}
